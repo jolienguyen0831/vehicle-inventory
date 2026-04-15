b2d9a5348ffc453af8548a95df1684f789ff1f34
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-15 15:49:09 -0500

    feat: preload 6 vehicles in the array

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -1,4 +1,59 @@
 package com.pluralsight;
 
+
 public class Vehicle {
+    private long vehicleID;
+    private String makeModel;
+    private String color;
+    private int odometerReading ;
+    private float price;
+
+    public Vehicle(long vehicleID, String makeModel, String color, int odometerReading, float price) {
+        this.vehicleID = vehicleID;
+        this.makeModel = makeModel;
+        this.color = color;
+        this.odometerReading = odometerReading;
+        this.price = price;
+    }
+
+    public long getVehicleID() {
+        return vehicleID;
+    }
+
+    public void setVehicleID(long vehicleID) {
+        this.vehicleID = vehicleID;
+    }
+
+    public String getMakeModel() {
+        return makeModel;
+    }
+
+    public void setMakeModel(String makeModel) {
+        this.makeModel = makeModel;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getOdometerReading() {
+        return odometerReading;
+    }
+
+    public void setOdometerReading(int odometerReading) {
+        this.odometerReading = odometerReading;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
+
--- src/main/java/com/pluralsight/VehicleInventory.java
+++ src/main/java/com/pluralsight/VehicleInventory.java
@@ -1,4 +1,31 @@
 package com.pluralsight;
 
-public class VehicleIventory {
+public class VehicleInventory {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[20];
+        int count = 0;
+        Vehicle vehicle1= new Vehicle(101121,"FordExplorer","Red",45000,13500);
+        vehicles[count]= vehicle1;
+        count++;
+        Vehicle vehicle2= new Vehicle(101122,"Toyota Camry","Blue",60000,11000);
+        vehicles[count]= vehicle2;
+        count++;
+        Vehicle vehicle3= new Vehicle(101123,"Chevrolet Malibu","Black",50000,9700);
+        vehicles[count]= vehicle3;
+        count++;
+        Vehicle vehicle4= new Vehicle(101124,"Honda Civic","White",70000,7500);
+        vehicles[count]= vehicle4;
+        count++;
+        Vehicle vehicle5 = new Vehicle(101125,"Subaru Outback","Green",55000,14500);
+        vehicles[count]= vehicle5;
+        count++;
+        Vehicle vehicle6 = new Vehicle(101126,"Jeep Wrangler","Yellow",30000,16000);
+        vehicles[count]= vehicle6;
+        count++;
+
+
+
+        }
+
+
 }
